be4482fad6ca847380eaa58640228bbfed9f11fb
Alexander Jaramillo <wjaramillo73@gmail.com>
2020-05-24 22:16:26 -0500

    Se inicia proyecto y se crea plantilla de Login

---
+++ FoodServiceMovil/FoodService/FoodService/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using FoodService.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodService.ViewModels
+{
+    public class LoginViewModel
+    {
+        public LoginModel Item = new LoginModel();
+        LoginViewModel(LoginModel item)
+        {
+            this.Item = item;
+        }
+    }
+}
